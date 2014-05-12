4f245780ae9f2c195e462ab8feb3752e5eca6e1f
Derek Donnelly <derek@derekdonnelly.com>
2014-05-12 22:37:09 +0100

    Load the watch npm task.

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-nodeunit");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
